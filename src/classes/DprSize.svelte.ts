120075b689ee936ed937ac80303a7a2e95b6678c
Josh Oertel <16182568+joshwashywash@users.noreply.github.com>
2026-07-29 17:21:32 -0500

    floor renderer size values

--- src/classes/DprSize.svelte.ts
+++ src/classes/DprSize.svelte.ts
@@ -4,7 +4,7 @@ export class DprSize {
 	width: number;
 	height: number;
 	constructor(width = () => 1, height = () => 1) {
-		this.width = $derived(width() * (devicePixelRatio.current ?? 1));
-		this.height = $derived(height() * (devicePixelRatio.current ?? 1));
+		this.width = $derived(Math.floor(width() * (devicePixelRatio.current ?? 1)));
+		this.height = $derived(Math.floor(height() * (devicePixelRatio.current ?? 1)));
 	}
 }
